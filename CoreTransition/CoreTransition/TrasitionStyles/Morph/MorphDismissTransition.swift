465b448642e5e2c248188fc262ea8cfc8fe4d25c
Ahmed Abdelkarim <ahmed.karim.tantawy@live.com>
2022-08-07 01:43:39 +0200

    fixed bug in morph dismiss (clip to bounds while dismiss)

--- CoreTransition/CoreTransition/TrasitionStyles/Morph/MorphDismissTransition.swift
+++ CoreTransition/CoreTransition/TrasitionStyles/Morph/MorphDismissTransition.swift
@@ -24,10 +24,20 @@ class MorphDismissTransition: CTTransition {
         // calculate animation values
         let dismissedViewFinalFrame = self.originView.superview?.convert(self.originView.frame, to: nil) ?? CGRect(x: 0, y: 0, width: 0, height: 0)
         
+        // set initial values
+        let clipsToBounds = dismissedView.clipsToBounds // save clipsToBounds to reset after completion
+        dismissedView.clipsToBounds = true
+        
         // setup animator
         let animator = UIViewPropertyAnimator(duration: self.transitionDuration(using: transitionContext), curve: .easeInOut) {
             dismissedView.frame = dismissedViewFinalFrame
             dismissedView.layer.cornerRadius = self.originView.layer.cornerRadius
+        }
+        
+        // reset state
+        transitionContext.resetState = {
+            // reset clipsToBounds to avoid changing normal state of VC
+            dismissedView.clipsToBounds = clipsToBounds
         }
         
         return animator
